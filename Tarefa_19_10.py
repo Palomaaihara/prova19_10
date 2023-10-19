0110a8cb483e4b69afd08514950fce41ca0706d6
MarcusBMDev <donmarcola26@gmail.com>
2023-10-19 20:51:44 -0300

    DELETAR EMPRESTIMO

--- Tarefa_19_10.py
+++ Tarefa_19_10.py
@@ -80,7 +80,22 @@ def deletar_pessoa(pessoas_csv, pessoas):
             writer.writerows(pessoas_no_arquivo)  # Escreve as pessoas restantes
 
         print("Pessoa(s) removida(s) com sucesso!")
+#DELETAR EMPRESTIMO
+def deleter_registro(emprestimos):
+    pesquisa = input("Digite o nome que deseja excluir: ")
+    emprestimos_para_remover = []
 
+    for emprestimo in emprestimos:
+        if emprestimo['EMPRESTIMO'] == pesquisa:
+            emprestimos_para_remover.append(emprestimo)
+
+    if not emprestimos_para_remover:
+        print("Nome não encontrado na lista!")
+    else:
+        for emprestimo in emprestimos_para_remover:
+            emprestimos.remove(emprestimo)
+
+        criar_csv() 
 #menu
 while True:
     print("------BIBLIOTECA------")
